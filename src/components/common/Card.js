285bede43bc2fc2e9afc3c618eae66402d6beaf3
sumukha.m <sumukhamohan6@gmail.com>
2022-07-16 13:46:12 +0530

    feat(card): added card actions support

--- src/components/common/Card.js
+++ src/components/common/Card.js
@@ -1,11 +1,19 @@
 import React from "react";
 import Card from "@material-ui/core/Card";
 import CardContent from "@material-ui/core/CardContent";
+import { CardActions } from "@material-ui/core";
 
-function CommonCard(props) {
-  const { title, imageUrl,imgAlt, children } = props;
+function CustomCard(props) {
+  const {
+    title,
+    imageUrl,
+    imgAlt,
+    children,
+    leftBtn = {},
+    rightBtn = {},
+  } = props;
   return (
-    <Card className="card">
+    <Card>
       <CardContent>
         {!!imageUrl && (
           <div>
@@ -13,10 +21,22 @@ function CommonCard(props) {
           </div>
         )}
         <div className="title">{title}</div>
-       <div>{children}</div>
+        <div>{children}</div>
       </CardContent>
+      <CardActions className="dsp-flex justify-content-end">
+        {leftBtn.required && (
+          <button className="btn-secondary" onClick={leftBtn.onClick}>
+            {leftBtn.label}
+          </button>
+        )}
+        {rightBtn.required && (
+          <button className="btn-primary" onClick={rightBtn.onClick}>
+            {rightBtn.label}
+          </button>
+        )}
+      </CardActions>
     </Card>
   );
 }
 
-export default CommonCard;
+export default CustomCard;
