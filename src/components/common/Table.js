763014b1f0f9332c7393e8ff1eee9d37d148702a
sumukha.m <sumukhamohan6@gmail.com>
2022-07-16 13:43:26 +0530

    feat(table): added common table component

---
+++ src/components/common/Table.js
@@ -0,0 +1,69 @@
+import React from "react";
+import { makeStyles, withStyles } from "@material-ui/core/styles";
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableContainer from "@material-ui/core/TableContainer";
+import TableHead from "@material-ui/core/TableHead";
+import TableRow from "@material-ui/core/TableRow";
+import Paper from "@material-ui/core/Paper";
+
+const StyledTableCell = withStyles((theme) => ({
+  head: {
+    backgroundColor: theme.palette.common.black,
+    color: theme.palette.common.white,
+  },
+  body: {
+    fontSize: 14,
+  },
+}))(TableCell);
+
+const StyledTableRow = withStyles((theme) => ({
+  root: {
+    "&:nth-of-type(odd)": {
+      backgroundColor: theme.palette.action.hover,
+    },
+  },
+}))(TableRow);
+
+const useStyles = makeStyles({
+  table: {
+    minWidth: 100,
+  },
+  container: {
+    maxHeight: "75vh",
+    overflowY: "auto",
+  },
+});
+
+function CustomTable(props) {
+  const classes = useStyles();
+  const { rows, tableHeader } = props;
+
+  return (
+    <TableContainer component={Paper} className={classes.container}>
+      <Table stickyHeader className={classes.table} aria-label="simple table">
+        <TableHead>
+          <StyledTableRow>
+            {tableHeader.map((col) => (
+              <StyledTableCell key={col.key}>{col.label}</StyledTableCell>
+            ))}
+          </StyledTableRow>
+        </TableHead>
+        <TableBody>
+          {rows.map((row) => (
+            <StyledTableRow key={row.id}>
+              {tableHeader.map((header) => (
+                <StyledTableCell key={header.key}>
+                  {row[header.key]}
+                </StyledTableCell>
+              ))}
+            </StyledTableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
+}
+
+export default CustomTable;
