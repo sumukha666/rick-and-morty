24d9f37604a282cc91352773bec690d5e7d98175
sumukha.m <sumukhamohan6@gmail.com>
2022-07-24 23:55:58 +0530

    feat(app): added header component to the page

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -2,10 +2,12 @@ import React from "react";
 import CharacterCard from "../Character/CharacterCard";
 import InfiniteScroller from "../common/InfiniteScroller";
 import { getCharactersAPI } from "../httpRequests/characters";
+import Header from "../common/Header";
 
 function Home() {
   return (
     <div>
+      <Header />
       <InfiniteScroller getDataAction={getCharactersAPI}>
         <CharacterCard />
       </InfiniteScroller>
---
+++ src/components/common/Header.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+
+const useStyles = makeStyles(() => ({
+  toolBar: {
+    minHeight: "3rem",
+  },
+  appBar: {
+    height: "3rem",
+  },
+  title: {
+    marginRight: "1rem",
+  },
+}));
+
+export default function ButtonAppBar() {
+  const classes = useStyles();
+
+  return (
+    <>
+      <AppBar className={classes.appBar} position="sticky">
+        <Toolbar className={classes.toolBar}>
+          <Typography variant="h6" className={classes.title}>
+            Rick and Morty
+          </Typography>
+        </Toolbar>
+      </AppBar>
+    </>
+  );
+}
